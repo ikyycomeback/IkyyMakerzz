1a4d51a77942f07d27d1fc32c2de178753fde6e9
ikyycomeback <131152222+ikyycomeback@users.noreply.github.com>
2023-04-18 21:55:33 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ loadDatabase()
         if (!ikybotz.public && !mek.key.fromMe && chatUpdate.type === 'notify') return
         if (mek.key.id.startsWith('BAE5') && mek.key.id.length === 16) return
         m = smsg(ikybotz, mek, store)
-        require("./jo")(ikybotz, m, chatUpdate, store)
+        require("./iky")(ikybotz, m, chatUpdate, store)
         } catch (err) {
             console.log(err)
         }
